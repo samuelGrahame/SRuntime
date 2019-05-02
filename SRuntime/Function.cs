9bd3f65a7839756ad034222f8744c710f718e102
samuel grahame <samuel@intelogy.com.au>
2019-05-02 15:57:50 +1000

    Add project files.

---
+++ SRuntime/Function.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SRuntime
+{
+    public unsafe struct Function
+    {
+        public fixed int ConstData[255];
+        public fixed byte Data[65535];        
+        fixed int registor[255];
+
+        public unsafe int Run()
+        {
+            int index = 0;
+
+            fixed (int* registor = this.registor)
+            fixed (int* ConstData = this.ConstData)
+            fixed (byte* Data = this.Data)
+            {
+                do
+                {
+                    
+                    switch (Data[index])
+                    {
+                        case 0: // LOAD LITERAL STORE IN REGISTOR - REGISTOR INDEX - COST INDEX
+                            registor[Data[++index]] = ConstData[Data[++index]];
+                            break;
+                        case 1: // ADD - DESTINATION - REG1 - REG2
+                            registor[Data[++index]] = registor[Data[++index]] + registor[Data[++index]];
+                            break;
+                        case 2: // SUB - DESTINATION - REG1 - REG2
+                            registor[Data[++index]] = registor[Data[++index]] - registor[Data[++index]];
+                            break;
+                        case 3: // DIV - DESTINATION - REG1 - REG2
+                            registor[Data[++index]] = registor[Data[++index]] / registor[Data[++index]];
+                            break;
+                        case 4: // MUL - DESTINATION - REG1 - REG2
+                            registor[Data[++index]] = registor[Data[++index]] * registor[Data[++index]];
+                            break;
+                        case 5: // PUSH FORWARD
+                            index += Data[++index];
+                            break;
+                        case 6: // PUSH BACKWARD
+                            index -= Data[++index];
+                            break;
+                        case 7: // RETURN
+                            return registor[Data[++index]];
+                        case 8: // SKIP FALSE
+                            if (registor[Data[++index]] != 0)
+                            {
+                                index++;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                } while (index++ < 65535);
+            }            
+
+            return 0;
+        }
+    }
+}
---
+++ SRuntime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SRuntime
+{
+    unsafe class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return 0;
+
+            Function app;
+
+            var data = System.IO.File.ReadAllBytes(args[0]);
+            var index = 0;
+            int i;
+
+            ushort length = data[index++];
+            for (i = 0; i < length; i++)
+                app.ConstData[i] = data[index++];
+
+            length = (ushort)(data[index++] + (data[index++] << 8));
+            for (i = 0; i < length; i++)
+                app.Data[i] = data[index++];
+
+            return app.Run();
+        }
+    }
+}
